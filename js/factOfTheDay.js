ce263a9154ea6afdb4df8c42438cb0602c7b1ad2
jhauga <johnhaugabook@gmail.com>
2023-12-13 17:22:10 -0500

    mind static pages

--- js/factOfTheDay.js
+++ js/factOfTheDay.js
@@ -10,7 +10,8 @@
   let wikiPromise = new Promise(
    function(yes, no) {
     let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "/API/factOfTheDay.php");
+    //xmlhttp.open("GET", "/API/factOfTheDay.php"); // if using from repo.
+    xmlhttp.open("GET", "/API/factOfTheDay-JSON.json");
     xmlhttp.onload = function() {
      if (xmlhttp.status == 200) {
       yes(xmlhttp.response);
@@ -21,7 +22,47 @@
     xmlhttp.send();
    }
   );
-  factOfTheDay.innerHTML = await wikiPromise;
+  return await wikiPromise;
  }
- // Call function.
- getfactOfTheDay();
+ // Call function and get output..
+ var output, result, pageID, pageTitle, extract_html,
+     error_out, extract_html;
+ getfactOfTheDay().then(function(outText) {
+  // convert output to json
+  result = JSON.parse(outText); 
+ 
+  // Travers json and get content.   
+  extract_html = "";
+  let getFeaturedArticle = function() { // START PROCESS - extract featured article section
+   pageID = result["tfa"]["pageid"];
+   pageTitle = result["tfa"]["title"];
+   // travers to get extract_html property
+   for (i in result) {
+    if (i == "extract_html") { extract_html += result[i]; }
+   }       
+  };
+  getFeaturedArticle(); 
+ 
+  // Check if info was returned.
+  let outputFeaturedArticle = function() {    
+   // check that main elments were returned
+   if (extract_html == undefined) {
+    error_out = 1;
+   } else {
+    error_out = 0;   
+   }
+  };
+  outputFeaturedArticle();
+  
+  // check to make sure output ok
+  if (error_out == 1) {
+   factOfTheDay.innerHTML = "Uh-oh - something went wrong.";  
+  } else {                // Output first paragraph of featured article.    
+  factOfTheDay.innerHTML = extract_html +   // Using str_replace() to clean extracted value.
+   " More at <a href='https://en.wikipedia.org/?curid=$pageID' target='_blank' rel='external'>" + 
+   pageTitle.replace(/_/g, " ") +
+   "</a> Wikipedia page. ";
+  }
+ });
+ 
+ 
